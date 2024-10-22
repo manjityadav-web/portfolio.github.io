2cda942500710ffcc0002058e7f92a4af2bad684
manjityadav-web <manjityadav9986@gmail.com>
2024-10-22 15:19:39 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed (".multiple-text", {
+    strings: ["Learning-Web Development & Software Development"],
+    typespeed: 20,
+    backspeed:20,
+    backdelay:500,
+    loop: true
+})
+
